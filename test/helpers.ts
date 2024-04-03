522ea560ae4a614a20437d6c86cd3448eba1f905
Ferran Llop <contacto@ferranllop.com>
2024-04-03 18:56:40 +0200

    Move valid config to test helpers file

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,6 +1,40 @@
 import fs from 'node:fs'
 import { Browser, Page } from 'puppeteer'
 
+export const validFullConfig = {
+    "puppeteer": {
+        "browserLanguage": "es-ES",
+        "sandboxBrowser": true,
+        "disableSetuidSandbox": true,
+        "headless": true,
+        "dumpio": true,
+        "timeout": 30000
+    },
+    "webs":[{
+        "activate": true,
+        "url": "https://www.google.com/maps/place/SOME+COMPANY/@40.2234903,2.4440167,17z/data=lotsofgibberish",
+        "provider": "google",
+        "ignoreReviews": {
+            "byAuthorName": ["John Doe", "Foo Bar"],
+            "byMinimumRating": 4,
+            "byMinimumCharactersCountInContent": 10
+        },
+        "known": {
+          "review": {
+              "authorName": "Lidia Gonzalez Pot",
+              "content": "¡Buen trato, buena faena, buen resultado! Recomendable",
+          },
+          "rejectCookiesButtonText": "Rechazar todo",
+          "viewMoreButtonText": "Más",
+          "viewUntranslatedContentButtonText": "Ver original",
+          "reviewsSectionButtonText": "Reseñas",
+          "sortingButtonText": "Ordenar",
+          "byNewestOptionButtonText": "Más recientes",
+          "oldestReviewAuthorName": "Q- Beat"
+        }
+    }]
+} 
+
 export const getAbsoluteFilePath = (prefix: string, suffix: string) => 
     (relativeFilePath: string) => new URL(prefix + relativeFilePath + suffix, import.meta.url)
 
