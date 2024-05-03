30bdcdfb2e269cec7b36a337e7bb26c2dc84128c
Ferran Llop <contacto@ferranllop.com>
2024-05-03 12:25:13 +0200

    Make run.sh be able to run multiple long running commands on the same container

--- run.sh
+++ run.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
+IMAGE_NAME="narciso-puppeteer"
+CONTAINER_NAME=narciso
+
 set -e
-IMAGE_NAME="scrapping-reviews-puppeteer"
+
 if [ -z "$(docker images -q $IMAGE_NAME 2> /dev/null)" ]; then
   docker build -t $IMAGE_NAME ./docker/
 fi
 
-docker run \
-	--init \
-	--cap-add SYS_ADMIN \
-	--workdir /app \
-	-v ./:/app \
-	-u $(id -u) \
-	--name scraping_reviews \
-	--restart no \
-	--rm \
-	-ti \
-	$IMAGE_NAME "$@"
+
+if [ -z "$(docker ps -q -f name=$CONTAINER_NAME 2> /dev/null)" ]; then
+  docker run \
+    --init \
+    --cap-add SYS_ADMIN \
+    --workdir /app \
+    -v ./:/app \
+    -u $(id -u) \
+    --name $CONTAINER_NAME \
+    --restart no \
+    --rm \
+    -ti \
+    $IMAGE_NAME "$@"
+else
+  docker exec -it $CONTAINER_NAME "$@"
+fi
