1b1a551a9afbf8b5986164c7f28ed7abcd103c42
Ferran Llop <contacto@ferranllop.com>
2024-09-23 20:40:09 +0200

    Refactor log formatter

--- src/logger/log-formatter.ts
+++ src/logger/log-formatter.ts
@@ -1,19 +1,22 @@
 export const standardFormat = (log: string[]) => createParagraphsOnLog(indentLog(log))
 
-export const indentLog = (logLines: string[], indentUnit = '\t') => 
-	logLines.map((line, currentLineIndex) => {
-		const startWordCount = countCoincidences(/^start:/i, logLines.slice(0, currentLineIndex+1))
-		const finishWordCount = countCoincidences(/^finish:/i, logLines.slice(0, currentLineIndex))
+export const indentLog = (logLines: string[], indentUnit = '\t') => {
+
+	const indentLine = (line: string, currentLineIndex: number) => {
+		const startWordCount = countLinesWith(/^start:/i, logLines.slice(0, currentLineIndex + 1))
+		const finishWordCount = countLinesWith(/^finish:/i, logLines.slice(0, currentLineIndex))
 		const indentationSize = /^(start|finish)/i.test(line) 
 			? startWordCount - finishWordCount - 1
 			: startWordCount - finishWordCount
 		const indentation = Array(indentationSize).fill(indentUnit).join('')
 		return indentation + line
-	})
+	}
 
-const countCoincidences = (regex: RegExp, lines: string[]) => 
-	lines.reduce((count, line) => regex.test(line) ? count + 1 : count, 0)
-	
+	const countLinesWith = (regex: RegExp, lines: string[]) => 
+		lines.reduce((count, line) => regex.test(line) ? count + 1 : count, 0)
+
+	return logLines.map(indentLine)
+}	
 
 
 export const createParagraphsOnLog = (logLines: string[], indentUnit = '.') => 
