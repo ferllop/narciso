1eeb6a104a27796ffb31baab6a540bc6dbf7bbd1
Ferran Llop <contacto@ferranllop.com>
2024-09-24 19:26:34 +0200

    Fix domain/config.ts being gitignored

---
+++ src/domain/config/config.ts
@@ -0,0 +1,51 @@
+import type { GoogleSpecificConfig } from '../scraper/providers/google/google.config.js'
+
+export type RawConfig = {
+	puppeteer: RawPuppeteerConfig
+	webs: RawWebConfig<Provider>[]
+}
+
+export type RawPuppeteerConfig = {
+	browserLanguage?: string
+	headless?: boolean
+	dumpio?: boolean
+	sandboxBrowser?: boolean,
+	disableSetuidSandbox?: boolean,
+}
+
+export type Provider = 'google' | 'bodasnet'
+export type RawWebConfig<P extends Provider> = {
+	title: string
+	activate: boolean
+	useInTests?: boolean
+	url: string 
+	timeout?: number
+	ignoreReviews?: RawIgnoreReviewsConfig 
+	provider: P 
+}  & SpecificConfig<P>
+
+export type RawIgnoreReviewsConfig = {
+	byAuthorName?: string[] 
+	byMinimumCharactersCountInContent?: number 
+	byMinimumRating?: number
+}
+
+type SpecificConfig<P extends Provider> = 
+	P extends 'google' ? { specific: GoogleSpecificConfig } :
+	{}
+
+export type Config = {
+	puppeteer: PuppeteerConfig
+	webs: WebConfig<Provider>[]
+}
+
+export type PuppeteerConfig = Omit<RawPuppeteerConfig, 'browserLanguage' | 'sandboxBrowser' | 'disableSetuidSandbox'> & {
+	args: string[]
+}
+
+export type WebConfig<P extends Provider> = RawWebConfig<P> & {
+	timeout: number
+	ignoreReviews: IgnoreReviewsConfig
+}
+
+export type IgnoreReviewsConfig = Required<RawIgnoreReviewsConfig>
